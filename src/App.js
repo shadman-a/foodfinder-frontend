e11604728c3224174940216b2e695486f81ade52
shadman-a <ahmedshadman966@gmail.com>
2021-01-25 10:12:22 -0500

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ export default class App extends React.Component{
   }
 
   fetchApi=()=>{
-    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${this.state.search}&latitude=${this.state.lat}&longitude=${this.state.lng}`,{
+    fetch(`https://api.yelp.com/v3/businesses/search?term=${this.state.search}&latitude=${this.state.lat}&longitude=${this.state.lng}`,{
         method: 'GET',
         headers: {
             "Authorization": `Bearer ${YELP_API_KEY}`
