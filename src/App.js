d705df8e2f3205d7e2fab8f35328fe8e108eac35
Shadman Ahmed <61790489+shadman-a@users.noreply.github.com>
2023-03-27 18:38:15 -0400

    Revert "dark theme"

--- src/App.js
+++ src/App.js
@@ -1,65 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import {
-  Paper,
-  Button,
-} from "@mui/material";
-import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-
-
+  Button
+} from "@material-ui/core";
 import { FaLocationArrow } from 'react-icons/fa'
 
 import Home from './Home'
 import Favorites from './Favorites';
 import Mapa from './Map'
 import FavoritesView from './FavoritesView'
+
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
 import Login from './components/Login';
 
-import Form from 'react-bootstrap/Form'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+export default class App extends React.Component{
 
+  state = {
+    search: "",
+    locationSearch: "",
+    buisnessArray: [],
+    lng: -73.935242,
+    lat: 40.730610,
+  };
 
-export default function App() {
-  const [search, setSearch] = useState("");
-  const [locationSearch, setLocationSearch] = useState("");
-  const [buisnessArray, setBuisnessArray] = useState([]);
-  const [lng, setLng] = useState(-73.935242);
-  const [lat, setLat] = useState(40.730610);
-  const [darkTheme, setDarkTheme] = useState(createTheme({
-    palette: {
-      mode: 'dark',
-    },
-  }));
+  componentDidMount(){
+    this.getLocation()
+    this.fetchApi()
+  }
 
-  useEffect(() => {
-    getLocation();
-    fetchApi();
-  });
-
-  const getLocation = () => {
+  getLocation=()=>{
     navigator.geolocation.getCurrentPosition(
-      (position) => {
-        let lat = position.coords.latitude
-        let lng = position.coords.longitude
-        setLat(lat);
-        setLng(lng);
-      },
-      (error) => {
-        console.error(JSON.stringify(error))
-      },
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
+        (position) => {
+          let lat = position.coords.latitude
+          let lng = position.coords.longitude
+          this.setState({
+            lat: lat,
+            lng: lng
+          })
+        },
+        (error) => {
+          console.error(JSON.stringify(error))
+        },
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+      );
+      this.fetchApi();
   }
 
   fetchApi=()=>{
@@ -75,75 +63,54 @@ export default function App() {
         }),
         redirect: 'follow'
     })
-      .then(response => response.json())
-      .then(result => {
-        setBuisnessArray(result.businesses);
-        setSearch("");
-      });
-  }
+    .then(response => response.json())
+    .then(result => this.setState({
+        buisnessArray: result.businesses
+    }),
+    this.setState({
+        search: ""
+    })
+)}
 
-  const handleChange = (event) => {
-    setSearch(event.target.value);
+  handleChange = (event) => {
+    this.setState({ search: event.target.value }); 
   };
 
+
+  render() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <>
-        <div>
-        <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-          >
-            🍔Food Finder
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          <div className="mr-auto">
-              <Button href="/">Home</Button>
-              <Button  href="/map">Map</Button >
-              <Button  href="/favorites">Favorites</Button >
-              {sessionStorage.getItem("jwt") === null ?
-                <Button  href="/login">Log In</Button > : <Button  href="/login">{sessionStorage.getItem("username")}</Button>}
-            </div>
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-          </Box>
-          <Form inline>
-              <Form.Control type="text" placeholder="Search" className="mr-sm-2"
-                value={search}
-                onChange={(event) => handleChange(event)} />
-              <Button variant="outline-info" onClick={() => fetchApi()}>Search</Button>
-              <Button>
-                <FaLocationArrow onClick={() => getLocation()} style={{
-                  fontSize: 16}}  />
-                  </Button>
-                </Form>
-        </Toolbar>
-      </AppBar>
-    </Box>
-              <Router>
-                <Route exact path="/" render={() => <Home lng={lng} lat={lat} buisnessArray={buisnessArray} />} />
-                <Route exact path="/map" render={() => <Mapa lng={lng} lat={lat} buisnessArray={buisnessArray} />} />
-                <Route exact path="/favorites" render={() => <Favorites />} />
-                <Route exact path="/fav" component={FavoritesView} />
-                <Route exact path="/login" component={Login} />
-              </Router>
-            </div>
-            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-            <BottomNavigation
-              showLabels
-            >
-              <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-              <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-              <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-            </BottomNavigation>
-            </Paper>
-          </>
-        </ThemeProvider>
-      );
-    }
+    <div>
+      <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="/">🍔Food Finder</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/map">Map</Nav.Link>
+      <Nav.Link href="/favorites">Favorites</Nav.Link>
+      {sessionStorage.getItem("jwt") === null ? 
+      <Nav.Link href="/login">Log In</Nav.Link> : <Nav.Link href="/login">{sessionStorage.getItem("username")}</Nav.Link>}
+    </Nav>
+    <Form inline>
+      <Form.Control type="text" placeholder="Search" className="mr-sm-2" 
+      value={this.state.search}
+      onChange={(event) => this.handleChange(event)}/>
+      <Button variant="outline-info" onClick={() => this.fetchApi()}>Search</Button>
+      <Button>
+      <FaLocationArrow onClick={()=>this.getLocation()} style={{fontSize: 16}} />
+      </Button>
+    </Form>
+  </Navbar>
+    <Router>
+          <Route exact path="/" render={() => <Home lng={this.state.lng} lat={this.state.lat} buisnessArray={this.state.buisnessArray}/>}  />
+          <Route exact path="/map" render={() => <Mapa lng={this.state.lng} lat={this.state.lat} buisnessArray={this.state.buisnessArray}/>} />
+          <Route exact path="/favorites" render={() => <Favorites />}/>
+          <Route exact path="/fav" component={FavoritesView}/>
+          <Route exact path="/login" component={Login}/>
+      </Router>
+
+  </div>
+
     
+  );
+}
+}
+
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Modal } from "react-bootstrap";
-import { Button } from "@mui/material";
+import { Button } from "@material-ui/core";
 
 class Login extends Component {
     constructor(props) {
